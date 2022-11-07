751d3b0b8e77eccea9a2e728875af433f2eb2686
David Josiger <david.josiger@gmail.com>
2022-11-07 11:08:11 +0100

    started working on main

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from collections import namedtuple
+import yaml
+import sys
+import helpers
+import time as t
 
-Link = namedtuple("Link", "article_title article_link")
+# importing scraper configuration
+CONFIG = yaml.safe_load(open("./config.yml"))
+START_YEAR = CONFIG["scraper_start_year"]
+END_YEAR = CONFIG["scraper_end_year"]
+START_ISSUE = CONFIG["scraper_start_issue"]
+END_ISSUE = CONFIG["scraper_end_issue"]
+WAIT = CONFIG["scraper_wait"]
 
-link = Link("gandalf", "penis")
+conf_inf = f"The scraper will run from {START_ISSUE}/{START_YEAR} to {END_ISSUE}/{END_YEAR}. Wait time between requests is {WAIT} seconds. Start the scraper? (y/n)"
+conf_check = input(conf_inf)
+if conf_check.lower() == "n":
+    sys.exit()
 
-print(link.article_link)
+
+for article in helpers.get_article_links(
+    START_YEAR, END_YEAR, START_ISSUE, END_ISSUE, WAIT
+):
+    temp = helpers.Article(article)
+    t.sleep(WAIT)
+    print(temp)
